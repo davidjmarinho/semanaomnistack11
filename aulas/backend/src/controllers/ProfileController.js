3b6652ac4c2e64dcff5d3b2879a6c7e5b04ca4e5
David Pereira <davidjmarinhpereira@gmail.com>
2020-03-26 00:50:37 -0300

    Back-End da Aplicacao(Atualizado)

--- aulas/backend/src/controllers/ProfileController.js
+++ aulas/backend/src/controllers/ProfileController.js
@@ -7,6 +7,7 @@ module.exports = {
 
         const ong_id = request.headers.authorization;
 
+        //
         const incidents = await connection('incidents').where('ong_id', ong_id).select('*');
 
         return response.json(incidents);
